64d7b275f60126b691bacff15fe4e71c3042af34
landos77 <luisorlandogomez@hotmail.com>
2024-10-14 00:27:30 -0500

    Actualizar pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,12 @@
 */
 //% color=190 weight=100 icon="\uf1ec" block="Basic Blocks"
 namespace screenMagic{
-    
+    /**
+     * Address  LEDs linearly row first
+     */
+    //% blockId=screenMagic
+    //% block="plot at index"
+    //% index.min=0 index.max=25
     export function plotAt(index:number):void{
         index |=0
         const y = Math.floor(index / 5);
