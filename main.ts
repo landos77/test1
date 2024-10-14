64a9482acbd74c68055be2b06841a94ee96c8de3
landos77 <luisorlandogomez@hotmail.com>
2024-10-14 00:35:49 -0500

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,12 @@
 * Address  LEDs linearly row first.
 */
 //% color=190 weight=100 icon="\uf1ec" block="screenMagic"
+//% blockId=screenMagic
+//% block="plot at $index"
+//% index.min=0 index.max=25
 namespace screenMagic{
-    /**
-     * 
-     */
-    //% blockId=screenMagic
-    //% block="plot at $index"
-    //% index.min=0 index.max=25
+    
+    
     export function plotAt(index:number):void{
         index |=0
         const y = Math.floor(index / 5);
