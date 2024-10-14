035fee7b06f88fa1cc583e164778d81334a12699
landos77 <luisorlandogomez@hotmail.com>
2024-10-14 00:13:42 -0500

    Actualizar main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ namespace screenMagic{
     /**
      * Address  LEDs linearly row first
      */
-    //% blockId=screenmagicplotat
+    //% blockId=screenMagic
     //% block="plot at index"
     //% index.min=0 index.max=25
     export function plotAt(index:number):void{
